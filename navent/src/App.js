0b518e72fcd91cd532cbda7fce4f7629252822fb
gon <gonbatalb@gmail.com>
2020-05-02 00:12:06 -0300

    inicio

--- navent/src/App.js
+++ navent/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Grid } from "@material-ui/core";
+
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- navent/src/store/store.js
+++ navent/src/store/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import reducer from "../store/reducers/index";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import { createLogger } from "redux-logger";
+// import thunkMiddleware from "redux-thunk";
+// import reducer from "../store/reducers/index";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(createLogger(), thunkMiddleware)),
-);
+// export default createStore(
+//   reducer,
+//   composeEnhancers(applyMiddleware(createLogger(), thunkMiddleware)),
+// );
